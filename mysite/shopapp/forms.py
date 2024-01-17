4dc0dbe73c05372d635051715b75571a9dd3ebae
Александр Сырысев <kyrator@bk.ru>
2024-01-17 14:00:02 +0300

    decided tasks module 8

--- mysite/shopapp/forms.py
+++ mysite/shopapp/forms.py
@@ -1,6 +1,14 @@
 from django import forms
 from django.core import validators
 from .models import Product, Order
+from django.contrib.auth.models import Group
+from django.forms import ModelForm
+
+
+class GroupForm(ModelForm):
+    class Meta:
+        model = Group
+        fields = ['name']
 
 
 class ProductForm(forms.ModelForm):
--- mysite/shopapp/urls.py
+++ mysite/shopapp/urls.py
@@ -1,14 +1,64 @@
 from django.urls import path
 
-from .views import shop_index, groups_list, products_list, orders_list, create_product, create_orders
+from .views import (
+    ShopIndexView,
+    GroupsListView,
+    ProductDetailsView,
+    ProductsListView,
+    OrdersListView,
+    OrderDetailsView,
+    ProductCreateView,
+    ProductUpdateView,
+    ProductDeleteView,
+    OrderCreateView,
+    OrderUpdateView,
+    OrderDeleteView,
+)
 
 app_name = "shopapp"
 
 urlpatterns = [
-    path("", shop_index, name="index"),
-    path("groups/", groups_list, name="groups_list"),
-    path("products/", products_list, name="products_list"),
-    path("orders/", orders_list, name="orders_list"),
-    path("products/create/", create_product, name="create_product"),
-    path("orders/create/", create_orders, name="create_orders"),
+    path("", ShopIndexView.as_view(), name="index"),
+    path("groups/", GroupsListView.as_view(), name="groups_list"),
+
+    #              """PRODUCTS"""
+    path("products/", ProductsListView.as_view(), name="products_list"),
+    path("products/<int:pk>/", ProductDetailsView.as_view(), name="product_details"),
+    path("products/create/", ProductCreateView.as_view(), name="product_create"),
+    path("products/<int:pk>/update/", ProductUpdateView.as_view(), name="product_update"),
+    path("products/<int:pk>/archive/", ProductDeleteView.as_view(), name="product_delete"),
+
+    #              """ORDERS"""
+    path("orders/", OrdersListView.as_view(), name="orders_list"),
+    path("orders/<int:pk>/", OrderDetailsView.as_view(), name="order_details"),
+    path("orders/create/", OrderCreateView.as_view(), name="order_create"),
+    path("orders/<int:pk>/update/", OrderUpdateView.as_view(), name="order_update"),
+    path("orders/<int:pk>/archive/", OrderDeleteView.as_view(), name="order_delete"),
 ]
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
